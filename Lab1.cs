c9bb544c1a91a885cd183644557c83a33b64f7c4
Sandkingpro <70577167+Sandkingpro@users.noreply.github.com>
2020-09-23 16:59:51 +0500

    Add files via upload

--- Lab1.cs
+++ Lab1.cs
@@ -1,26 +1,27 @@
 ﻿using System;
 using System.Linq;
+using System.Xml.Linq;
 //Вариант 3
 namespace Array
 {
     public class Array
     {
-        public int k;//количество элементов
+        public int size;//количество элементов
         public string[] elem;//обращение к элементу
         public Array CreateArray()
         {
             Console.WriteLine("Введите количество ячеек массива");
             try
             {
-                this.k = Convert.ToInt32(Console.ReadLine());
+                this.size = Convert.ToInt32(Console.ReadLine());
             }
             catch (FormatException)
             {
                 Console.WriteLine("Вы ввели не целое число");
                 return CreateArray();
             }
-            this.elem = new string[k];
-            for (int i = 0; i < k; i++)
+            this.elem = new string[size];
+            for (int i = 0; i < size; i++)
             {
                 Console.Write("Элемент{0}=", i + 1);
                 this.elem[i] = Console.ReadLine();
@@ -29,7 +30,7 @@ namespace Array
         }
         static void PrintArray(Array array)
         {
-            for (int i = 0; i < array.k; i++)
+            for (int i = 0; i < array.size; i++)
             {
                 Console.WriteLine("Элемент{0}={1} ", i + 1, array.elem[i]);
             }
@@ -40,7 +41,7 @@ namespace Array
             {
                 Console.WriteLine("Введите индекс не превышающий количество ячеек в массиве");
                 int i = Convert.ToInt32(Console.ReadLine());
-                if (i >= array.k)
+                if (i >= array.size)
                 {
                     return ToIndexPrint(array);
                 }
@@ -57,7 +58,7 @@ namespace Array
         {
             Console.Write("Введите начальную комбинацию символов:");
             string len = Console.ReadLine();
-            for (int i = 0; i < array.k; i++)
+            for (int i = 0; i < array.size; i++)
             {
                 if (array.elem[i].StartsWith(len))
                 {
@@ -67,60 +68,121 @@ namespace Array
         }
         static void Intersection(Array array1, Array array2)
         {
-            var array = array1.elem.Intersect(array2.elem);
-            Console.WriteLine("Пересечение массивов:");
-            foreach (string s in array)
+            Array array = new Array();
+            int t = 0 ;
+            if (array1.size>=array2.size)
             {
+                array.elem = new string[array2.size];
+                
+            }
+            else
+            {
+                array.elem = new string[array1.size];
+            }
+            for (int i = 0; i < array1.size; i++)
+            {
+                for (int j = 0; j < array2.size; j++)
+                {
+                    if (array1.elem[i] == array2.elem[j] & !(array.elem.Contains(array1.elem[i])))
+                    {
+                        array.elem[t] = array1.elem[i];
+                        t += 1;
+                        continue;
+                    }
+                }
+            }
+            array.size = t ;
+            Console.WriteLine("Пересечение массивов:");
+            for (int i = 0; i < array.size; i++)
+            {
+                string s = array.elem[i];
                 Console.WriteLine(s);
             }
-
         }
         static void Union(Array array1, Array array2)
         {
-            var array = array1.elem.Union(array2.elem);
-            Console.WriteLine("Объединение массива");
-            foreach (string s in array)
+            string t;
+            Array array = new Array();
+            array.size = array1.size + array2.size;
+            array.elem = new string[array.size];
+            for(int i=0;i<array1.size;i++)
             {
+                array.elem[i] = array1.elem[i];
+            }
+            for(int i=array1.size;i<array.size;i++)
+            {
+                array.elem[i] = array2.elem[i+array2.size-array.size];
+            }
+            for (int i=0;i<array.size;i++)
+            {
+                for(int j=i+1;j<array.size;j++)
+                {
+                    if(array.elem[i]==array.elem[j])
+                    {
+                        array.elem[j] = null;
+                        if(j==array.size-1)
+                        {
+                            array.size -= 1;
+                            j -= 1;
+                        }
+                        else
+                        {
+                            for(int p=j;p<array.size-1;p++)
+                            {
+                                t = array.elem[p];
+                                array.elem[p] = array.elem[p+1];
+                                array.elem[p + 1] = t;
+                            }
+                            array.size -= 1;
+                            j -= 1;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Объединение массивов:");
+            for (int i = 0; i < array.size; i++)
+            {
+                string s = array.elem[i];
                 Console.WriteLine(s);
             }
         }
         static void Clutch(Array array1, Array array2)
         {
             Array array = new Array();
-            if (array1.k >= array2.k)
+            if (array1.size >= array2.size)
             {
-                array.k = array1.k;
+                array.size = array1.size;
             }
             else
             {
-                array.k = array2.k;
+                array.size = array2.size;
             }
-            array.elem = new string[array.k];
-            if (array1.k > array2.k)
+            array.elem = new string[array.size];
+            if (array1.size > array2.size)
             {
-                for (int i = 0; i < array2.k; i++)
+                for (int i = 0; i < array2.size; i++)
                 {
                     array.elem[i] = array1.elem[i] + array2.elem[i];
                 }
-                for (int i = array2.k; i < array1.k; i++)
+                for (int i = array2.size; i < array1.size; i++)
                 {
                     array.elem[i] = array1.elem[i];
                 }
             }
-            if (array1.k < array2.k)
+            if (array1.size < array2.size)
             {
-                for (int i = 0; i < array1.k; i++)
+                for (int i = 0; i < array1.size; i++)
                 {
                     array.elem[i] = array1.elem[i] + array2.elem[i];
                 }
-                for (int i = array1.k; i < array2.k; i++)
+                for (int i = array1.size; i < array2.size; i++)
                 {
                     array.elem[i] = array2.elem[i];
                 }
             }
-            if (array1.k == array2.k)
+            if (array1.size == array2.size)
             {
-                for (int i = 0; i < array1.k; i++)
+                for (int i = 0; i < array1.size; i++)
                 {
                     array.elem[i] = array1.elem[i] + array2.elem[i];
                 }
@@ -259,7 +321,7 @@ namespace Array
                         while (true)
                         {
                             Console.WriteLine("Введите данные с экрана:");
-                            Console.WriteLine("1-перейти в следущее меню.");
+                            Console.WriteLine("1-перейти в следущее меню, для работы с обоими массивами.");
                             Console.WriteLine("0-вернуться в предыдущее меню,чтобы пересоздать второй массив.");
                             Console.WriteLine("-1-вернуться в начальное меню.");
                             a = Console.ReadLine();
